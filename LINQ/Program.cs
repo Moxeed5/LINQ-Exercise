170eb711355f34207a7ab5f0bad44fd82a90768e
Max Morgan <maxmmorgan93@gmail.com>
2022-12-20 11:24:00 -0600

    Exercis 2 commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace LINQ
+﻿using System.Collections.Concurrent;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Gamelist = new List<string>();
+
+            Gamelist.Add("Mario");
+
+            Gamelist.Add("Halo");
+
+            Gamelist.Add("Fifa");
+
+            Gamelist.Add("Fortnite");
+
+            Gamelist.Add("GeoWars");
+
+            var orderedByLength = Gamelist.OrderBy(x => x.Length);
+
+            foreach(var item in orderedByLength)
+            {
+                Console.WriteLine(item);
+            }
+
+            
         }
     }
 }
